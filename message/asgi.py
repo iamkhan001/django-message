d5a454f304585b157d42296eee71b20bacd93d29
Imran Khan <iamkhan007@outlook.com>
2021-08-16 14:10:13 +0530

    first commit

--- message/asgi.py
+++ message/asgi.py
@@ -7,20 +7,24 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
-import os
-
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
+import api_backend.routing
+from django.conf.urls import url
 import chat.routing
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'message.settings')
 
+django_asgi_app = get_asgi_application()
+
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
+    "http": django_asgi_app,
+    'websocket': AuthMiddlewareStack(
         URLRouter(
-            chat.routing.websocket_urlpatterns
+            api_backend.routing.websocket_urlpatterns
         )
-    ),
+    )
 })
