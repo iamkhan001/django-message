b5fdef915268abe65b6221d8c559361126dfd972
imran khan <impathan007@gmail.com>
2021-08-25 03:54:59 +0000

    test

--- message/asgi.py
+++ message/asgi.py
@@ -1,30 +1,19 @@
-"""
-ASGI config for message project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
-"""
-
-import api_backend.routing
-from django.conf.urls import url
-import chat.routing
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
 import os
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'message.settings')
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+import chat.routing
 
-django_asgi_app = get_asgi_application()
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "message.settings")
 
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    'websocket': AuthMiddlewareStack(
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
         URLRouter(
-            api_backend.routing.websocket_urlpatterns
+            chat.routing.websocket_urlpatterns
         )
-    )
+    ),
 })
+
+
